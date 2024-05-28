4cdcbf691f1163251c84a148f906e645fad31dd8
Evert van de Waal <evert.van.de.waal@nobleo.nl>
2024-05-28 12:52:33 +0200

    Fixed problems in the model_modelling demo.

--- demos/model_modeling/model_specification_spec.py
+++ demos/model_modeling/model_specification_spec.py
@@ -7,7 +7,9 @@ from model_definition import (ModelDefinition, required,
 md = ModelDefinition()
 md.ModelVersion('0.1')
 
-
+available_shapes = "bar box circle closedcircle cloud component diamond document drum ellipse folder hexagon hourglass label note obliquerect octagon rect ringedclosedcircle square stickman tape triangledown triangleup tunnel"
+available_line_endings = "arrow halfarrow pointer triangle diamond longdiamond square arrowopen halfarrowopen pointeropen triangleopen diamondopen longdiamondopen squareopen hat linearror halflinearror one only_one zero_or_one many zero_or_many"
+available_port_shapes = "square cup connector circle half_circle triangle_in triangle_out diamond"
 
 ###############################################################################
 ## Logical model
@@ -34,23 +36,24 @@ class Anchor:
 ## Entities for diagrams
 
 @md.Entity(styling="shape:rect", parents=['SpecificationDiagram'])
-class ModelEntity:
+class ModelingEntity:
     name: str
     description: (longstr, detail)
-    block_shape: str
-    default_styling: str
-    styling: str
+    block_shape: selection(available_shapes)
+    entity_styling: str
 
 @md.Entity(styling="shape:rect", parents=['SpecificationDiagram'])
 class RelationshipEntity:
     name: str
-    styling: str
+    start_shape: selection(available_line_endings)
+    end_shape: selection(available_line_endings)
+    entity_styling: str
     
 @md.Entity(styling = "shape:rect;structure:Block;icon:square-full", parents=['SpecificationDiagram'])
 class PortEntity:
     name: str
     description: (longstr, detail)
-    styling: str
+    entity_styling: str
 
 @md.Entity(styling='shape:document')
 class Attribute:
@@ -75,25 +78,23 @@ class CompoundEntity:
     name: str
     description: (longstr, detail)
     block_shape: str
-    default_styling: str
-    styling: str
+    entity_styling: str
 
 @md.Entity(styling='shape:rect', parents=['SpecificationDiagram'])
 class InstanceEntity:
     name: str
     description: (longstr, detail)
     block_shape: str
-    default_styling: str
-    styling: str
+    entity_styling: str
 
 @md.Entity(styling='shape:folder', parents=['SpecificationDiagram'])
 class HierarchyEntity:
     name: str
     description: (longstr, detail)
-    styling: str
+    entity_styling: str
 
 
-all_entities = [ModelEntity, RelationshipEntity, PortEntity, InstanceEntity, CompoundEntity, HierarchyEntity, DiagramEntity]
+all_entities = [ModelingEntity, RelationshipEntity, PortEntity, InstanceEntity, CompoundEntity, HierarchyEntity, DiagramEntity]
 
 
 @md.CompoundEntity(
@@ -144,12 +145,12 @@ class SelectionOptionLink:
 class DiagramEntityLink:
     pass
 
-@md.Relationship(styling = "endmarker:diamond", source=[ModelEntity, CompoundEntity], target=[PortEntity])
+@md.Relationship(styling = "endmarker:diamond", source=[ModelingEntity, CompoundEntity], target=[PortEntity])
 class PortEntityLink:
     pass
 
 
-@md.Relationship(styling = "endmarker:hat;startmarker:square", source=[ModelEntity, CompoundEntity, PortEntity], target=[InstanceEntity])
+@md.Relationship(styling = "endmarker:hat;startmarker:square", source=[ModelingEntity, CompoundEntity, PortEntity], target=[InstanceEntity])
 class EntityInstantation:
     pass
 
